97257966ac249130ed7e63155fe2c1664594a194
Victor Hugo Braguim Canto <vbraguimcanto@gmail.com>
2024-10-22 11:17:07 -0300

    feat: add files from lab 02

---
+++ lab-02/run.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@db/dbname'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    new_user = User(name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added'}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name} for user in users])
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=False)
